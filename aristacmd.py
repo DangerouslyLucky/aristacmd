de4c95a6a3dd3092c1e950941720337d59f849f5
Scott Cuthbert <scottmichaelcuthbert@gmail.com>
2017-12-03 20:41:56 -0800

    what changed?

--- aristacmd.py
+++ aristacmd.py
@@ -4,7 +4,7 @@
 @author: Scott Cuthbert
 
 This script is designed for Arista switches.
-Attempting to recreate the functionality of 'ciscocmd7k' without using expect
+Attempting to recreate the functionality of ciscocmd without using expect
 
 Allows input of a single command or multiple commands (via textfile) to be
 run against a single switch or multiple switches (via textfile)
